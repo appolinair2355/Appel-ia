3ea76c5349f60ae1b97a15a353add9a62c8219a8
appolinair2355 <felicitationsbien990@gmail.com>
2026-01-31 09:56:39 +0100

    Create faq.js

---
+++ data/faq.js
@@ -0,0 +1,22 @@
+export const FAQ = [
+  {
+    question: "bonjour",
+    answer: "Bonjour 😊 Comment puis-je vous aider ?"
+  },
+  {
+    question: "prix",
+    answer: "Nos services commencent à partir de 2 000 FCFA."
+  },
+  {
+    question: "essai",
+    answer: "Oui, vous avez une période d’essai gratuite."
+  },
+  {
+    question: "contact",
+    answer: "Vous pouvez nous contacter via WhatsApp ou Telegram."
+  },
+  {
+    question: "cloud",
+    answer: "Le cloud est un système qui permet de stocker et utiliser des données via Internet."
+  }
+];
